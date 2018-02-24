c8b14877c0cdd80ee9fbab02cca48e0186d280b3
Tarık Ünal <36507033+Tarikunal@users.noreply.github.com>
2018-02-24 12:11:44 +0300

    Add files via upload

---
+++ sayıtahmin.py
@@ -0,0 +1,21 @@
+_auther_='Tarık Ünal' #24.02.17
+from random import randint
+
+rand = randint(1, 200)
+sayac = 0
+
+while True:
+    sayac += 1
+    sayi = int(input("1 ile 200 arasında değer girin (0 çıkış):"))
+    if (sayi == 0):
+        print("Program kapatılıyor!!!")
+        break
+    elif sayi < rand:
+        print("Yükselt.")
+        continue
+    elif sayi > rand:
+        print("Düşür.")
+        continue
+    else:
+        print("Rastele seçilen sayı {0}!".format(rand))
+        print("Tahmin sayınız {0}".format(sayac))
