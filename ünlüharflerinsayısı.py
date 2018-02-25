8d5c0aef010b00b634a9e9e303e3edc509f88406
Tarık Ünal <36507033+Tarikunal@users.noreply.github.com>
2018-02-25 15:00:46 +0300

    Add files via upload

---
+++ ünlüharflerinsayısı.py
@@ -0,0 +1,9 @@
+_auther_='Tarık Ünal'
+#Girilen metindeki ünlü harf sayısıını veren fonksiyon
+s = input("Metni giriniz...:")
+sesli_harf = 'aeıioöuüAEIİUÜOÖ'
+sayac = 0
+for i in s:
+    if i in sesli_harf:
+        sayac += 1
+print("--%s-- metnindeki sesli harf sayisi:%d" % (s,sayac))
