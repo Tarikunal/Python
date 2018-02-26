98d54aad1bd04a5c190a3b49b1b4338df7cef4e5
Tarık Ünal <36507033+Tarikunal@users.noreply.github.com>
2018-02-26 10:17:14 +0300

    Add files via upload

---
+++ Faktöriyel.py
@@ -0,0 +1,13 @@
+faktoriyel=1
+
+while True:
+    sayi=input("Pozitif bir sayı giriniz....")
+    if (int(sayi) < 0):
+        print("Yanlış bilgi girişi yaptınız....")
+    else:
+        for i in range(1,int(sayi)+1):
+            faktoriyel=faktoriyel*i
+
+        print("Faktröiyel sonucu =",faktoriyel)
+        break
+
