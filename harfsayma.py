9625497c88a9afdf07ac96edb79de7aedbc760e8
Tarık Ünal <36507033+Tarikunal@users.noreply.github.com>
2018-02-28 17:35:32 +0300

    Add files via upload

---
+++ harfsayma.py
@@ -0,0 +1,13 @@
+#Tarık Ünal
+#Harf/Kelime sayma
+metin = input("Metni giriniz...:")
+
+aranan_harf = input("Sayılacak harfi/kelimeyi giriniz: ")
+
+sonuc = 0
+
+for harf in metin:
+	if aranan_harf == harf:
+		sonuc += 1
+
+print(aranan_harf + " harfinden/kelimesinden metinde "  + str(sonuc) + " tane vardır")
