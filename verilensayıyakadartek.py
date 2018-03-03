1073cc00e9f6635e564bd5233f751552f7531b67
Tarık Ünal <36507033+Tarikunal@users.noreply.github.com>
2018-03-03 15:45:20 +0300

    Add files via upload

---
+++ verilensayıyakadartek.py
@@ -0,0 +1,10 @@
+s = input('Lütfen bir tam sayı giriniz..:')
+try:
+    if int(s)>=0:
+        print("Yazdığınız sayıya kadar olan tek sayılar:")
+        for i in range(int(s),0,-1):
+            if i%2 != 0:
+                print(i)
+        print("__________")
+except:
+    print("Lütfen bir sayı giriniz...:")
