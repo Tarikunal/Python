8b2cfe5388d9305fe9062089db744adcd40c7f15
Tarık Ünal <36507033+Tarikunal@users.noreply.github.com>
2018-03-03 15:42:36 +0300

    Add files via upload

---
+++ verilensayıyakadarçiftyazan.py
@@ -0,0 +1,10 @@
+s = input('Lütfen bir tam sayı yazınız..:\n')
+try:
+    if int(s)>=0:
+        print("Yazdığınız sayıya kadar olan çift sayılar:")
+        for i in range(int(s)):
+            if i % 2 == 0:
+                print(i)
+        print("__________")
+except:
+        print("LÜtfen bir tam sayı yazınız")
