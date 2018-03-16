89dd849ef6781c3b8018a94893ea13cd84b7871a
Tarık Ünal <36507033+Tarikunal@users.noreply.github.com>
2018-03-16 22:34:37 +0300

    Create dikdörtgenoluşturma.py

---
+++ dikdörtgenoluşturma.py
@@ -0,0 +1,12 @@
+def f(e):
+    print( e*" _")
+
+def g(h):
+    print ((h-1)*("|"+(2*a-1)*" "+"|\n")+"|"+(a-1)*"_ "+"_"+"|")
+    print ("ALAN=%s, ÇEVRE=%s" %(a*b, 2*(a+b)))
+
+a=int(input("Yan Kenar:"))
+b=int(input("Dik Kenar:"))
+
+f(a)
+g(b)
