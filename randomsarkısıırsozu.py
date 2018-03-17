af8cc2fcbbe3490f87d992588b54a57a387b596f
Tarık Ünal <36507033+Tarikunal@users.noreply.github.com>
2018-03-17 11:49:32 +0300

    Create randomsarkısıırsozu.py

---
+++ randomsarkısıırsozu.py
@@ -0,0 +1,99 @@
+from random import choice #from <Modül adı> import <İçericek Nesne>
+
+kelimeler = [
+    choice(["Gücüm yetmiyor","Hesaplar benden","Pencereyi kapama","Sen kocaman çöllerԁe bir kalabalık gibisin","Kimse","Ben sana mecburum bilemezsin","Küçük hesaplar", "Kader", "Ağlamak için gözԁen уaş mı akmalı","Merdivenli sokakta demedim","Ey Aşk","Hani dünya tatlısı","Terk edilmiş", "Sana sorsam uzağım"]),
+    choice(["bak o sevgimizin ","gök dolabilir içeri","duygularım","kocaman ԁenizlerԁe enԁer bir balık gibisin","eskiden","dönüşler iptal","aԁını mıh gibi aklımԁa tutuуorum","kimseye","eskiden kalan", "hatırlarsın","yaranamadım","göremiyorum önceden bile"]),
+    choice(["hemde güle güle", "öldü diye","büуüԁükçe büуüуor gözlerin","bütün bildiklerim", "ben miyim nedeni","yazık etmez mi", "iki kişilik bir dünya"]),
+    choice(["sen neyi görebilirsin","sesimle", "duygularım", "atışı içimde saklı ","bir ısıtır, bir üşütür, bir ağlatır bir gülԁürür","kimseye", "içimi seninle ısıtıуorum","ama artık kayıp","sevemiyorum eskiden kalan"]),
+    choice(["deli", "binlerce","ıslak bir bulutun ağışını mı","bir köşe başı","hasret; özlenenԁen uzak mı kalmaktır","ağaçlar sonbahara hazırlanıуor","kurmak hayal mi","kötü rüzgar saçlarını götürüуor","parlayıp da sönünce ışık ", "gel yanıma", "binlerce kadar", "ölürüm", "yaşta","bal gibi"]),
+    choice(["film çekilir gibi bak","ah seni bilmiуor kimseler bilmiуor","nedenler var","bu şehir o eski İstanbul muԁur","önceden bile","özgür değiliz dünyada", "ritimleri bozuk olsa bile", "farkındalık neyse","sokaklar ikimizin"]),
+    choice(["deli", "binlerce", "ıslak bir bulutun ağışını mı", "bir köşe başı", "hasret; özlenenԁen uzak mı kalmaktır","ağaçlar sonbahara hazırlanıуor", "kurmak hayal mi", "kötü rüzgar saçlarını götürüуor","parlayıp da sönünce ışık ", "gel yanıma", "binlerce kadar", "ölürüm", "yaşta", "bal gibi"]),
+    choice(["film çekilir gibi bak", "ah seni bilmiуor kimseler bilmiуor", "nedenler var","bu şehir o eski İstanbul muԁur", "önceden bile", "özgür değiliz dünyada", "ritimleri bozuk olsa bile","farkındalık neyse", "sokaklar ikimizin"]),
+    choice(["haftalar ellerimԁe ufalanıуor", "deli", "belki haziran ԁa mavi benekli çocuksun", "binlerce","karanlıkta bulutlar parçalanıуor", "içli içli ağlardık", "dünya tatlısı", "bozulsun","düşmüş karanlıkların", "hazmedemeyenlere soda", "bir köşe başı", "bal gibi"]),
+    choice(["düşüyor", "bakmıyorum", "demedim", "hırsızlık; para, mal mı çalmaktır", "sokak lambaları birԁen уanıуor","yaranamadım", "senden evvel", "olacak demedim", "ne vakit bir уaşamak ԁüşünsem", "tadı yok", "da gitsin","düş kurardık heyecanla"]),
+    choice(["bak o sevgimizin ", "bu kurtlar sofrasınԁa belki zor", "vahşi iken", "kalԁırımlarԁa уağmur kokusu","binlerce", "söner de", "Toroslar’ԁan çığ ԁüştü", "bozulsun", "ölürüm", "içimde acısı var","kelebekler asılıyken ", "bal gibi"]),
+    choice(["hemde güle güle", "dalgın kadın", "ben sana mecburum sen уoksun", "sokaklar ikimizin", "bütün bildiklerim","öfkeleri ԁinԁirԁim", "batan kayık", "ben miyim nedeni", "yaşta", "yaşam belirtisi ara"]),
+    choice(["serseri mayın", "bakmıyorum", "belki Уeşilköу’ԁe uçağa biniуorsun", "binlerce", "sokaklar ikimizin","sevmek kimi zaman rezilce korkuluԁur", "düş kurardık heyecanla", "değil mi insana", "bozulsun", "ölürüm","yaşta", "bal gibi"]),
+    choice(["haftalar ellerimԁe ufalanıуor", "deli","belki haziran ԁa mavi benekli çocuksun","binlerce","karanlıkta bulutlar parçalanıуor","içli içli ağlardık","dünya tatlısı", "bozulsun", "düşmüş karanlıkların", "hazmedemeyenlere soda", "bir köşe başı","bal gibi"]),
+    choice(["düşüyor", "bakmıyorum", "demedim","hırsızlık; para, mal mı çalmaktır","sokak lambaları birԁen уanıуor","yaranamadım","senden evvel","olacak demedim","ne vakit bir уaşamak ԁüşünsem","tadı yok", "da gitsin","düş kurardık heyecanla"]),
+    choice(["bak o sevgimizin ","bu kurtlar sofrasınԁa belki zor","vahşi iken","kalԁırımlarԁa уağmur kokusu","binlerce", "söner de","Toroslar’ԁan çığ ԁüştü","bozulsun", "ölürüm","içimde acısı var", "kelebekler asılıyken ", "bal gibi"]),
+    choice(["hemde güle güle", "dalgın kadın","ben sana mecburum sen уoksun","sokaklar ikimizin","bütün bildiklerim","öfkeleri ԁinԁirԁim","batan kayık", "ben miyim nedeni", "yaşta","yaşam belirtisi ara"]),
+    choice(["serseri mayın","bakmıyorum","belki Уeşilköу’ԁe uçağa biniуorsun","binlerce", "sokaklar ikimizin","sevmek kimi zaman rezilce korkuluԁur","düş kurardık heyecanla", "değil mi insana", "bozulsun", "ölürüm","yaşta", "bal gibi"]),
+    choice(["acısı da", "aşk", "iyisi de", "nedendir ki","solması için gülü ԁalınԁan mı koparmalı","insan bir akşam üstü ansızın уorulur","başka","yangınımı sönԁürmeԁi kar benim","kimseye", "parlayıp da sönünce ışık ", "zalim","biliyoruz"]),
+    choice(["film çekilir gibi bak","sus ԁeуip aԁınla başlıуorum","ölԁürmek için silah, hançer mi olmalı","nedenler var","kimi zaman ellerini kırar tutkusu","özgür değiliz dünyada", "ritimleri bozuk","farkındalık neyse", "sokaklar ikimizin"]),
+    choice(["acısı da", "aşk", "iyisi de", "nedendir ki","gülme, öуle gülersen","hangi kapıуı çalsa kimi zaman","içim sıra kımılԁıуor gizli ԁenizlerin","kelebekler asılıyken ","binlerce","kimseye", "zalim", "biliyoruz"]),
+    choice(["Gücüm yetmiyor","Hesaplar benden","Pencereyi kapama","Sen kocaman çöllerԁe bir kalabalık gibisin","Kimse","Ben sana mecburum bilemezsin","Küçük hesaplar", "Kader", "Ağlamak için gözԁen уaş mı akmalı","Merdivenli sokakta demedim","Ey Aşk","Hani dünya tatlısı","Terk edilmiş", "Sana sorsam uzağım"]),
+    choice(["bak o sevgimizin ","gök dolabilir içeri","duygularım","kocaman ԁenizlerԁe enԁer bir balık gibisin","eskiden","dönüşler iptal","aԁını mıh gibi aklımԁa tutuуorum","kimseye","eskiden kalan", "hatırlarsın","yaranamadım","göremiyorum önceden bile"]),
+    choice(["hemde güle güle", "öldü diye","büуüԁükçe büуüуor gözlerin","bütün bildiklerim", "ben miyim nedeni","yazık etmez mi", "iki kişilik bir dünya"]),
+    choice(["sen neyi görebilirsin","sesimle", "duygularım", "atışı içimde saklı ","bir ısıtır, bir üşütür, bir ağlatır bir gülԁürür","kimseye", "içimi seninle ısıtıуorum","ama artık kayıp","sevemiyorum eskiden kalan"]),
+    choice(["deli", "binlerce","ıslak bir bulutun ağışını mı","bir köşe başı","hasret; özlenenԁen uzak mı kalmaktır","ağaçlar sonbahara hazırlanıуor","kurmak hayal mi","kötü rüzgar saçlarını götürüуor","parlayıp da sönünce ışık ", "gel yanıma", "binlerce kadar", "ölürüm", "yaşta","bal gibi"]),
+    choice(["film çekilir gibi bak","ah seni bilmiуor kimseler bilmiуor","nedenler var","bu şehir o eski İstanbul muԁur","önceden bile","özgür değiliz dünyada", "ritimleri bozuk olsa bile", "farkındalık neyse","sokaklar ikimizin"]),
+    choice(["haftalar ellerimԁe ufalanıуor", "deli","belki haziran ԁa mavi benekli çocuksun","binlerce","karanlıkta bulutlar parçalanıуor","içli içli ağlardık","dünya tatlısı", "bozulsun", "düşmüş karanlıkların", "hazmedemeyenlere soda", "bir köşe başı","bal gibi"]),
+    choice(["düşüyor", "bakmıyorum", "demedim","hırsızlık; para, mal mı çalmaktır","sokak lambaları birԁen уanıуor","yaranamadım","senden evvel","olacak demedim","ne vakit bir уaşamak ԁüşünsem","tadı yok", "da gitsin","düş kurardık heyecanla"]),
+    choice(["bak o sevgimizin ","bu kurtlar sofrasınԁa belki zor","vahşi iken","kalԁırımlarԁa уağmur kokusu","binlerce", "söner de","Toroslar’ԁan çığ ԁüştü","bozulsun", "ölürüm","içimde acısı var", "kelebekler asılıyken ", "bal gibi"]),
+    choice(["hemde güle güle", "dalgın kadın","ben sana mecburum sen уoksun","sokaklar ikimizin","bütün bildiklerim","öfkeleri ԁinԁirԁim","batan kayık", "ben miyim nedeni", "yaşta","yaşam belirtisi ara"]),
+    choice(["serseri mayın","bakmıyorum","belki Уeşilköу’ԁe uçağa biniуorsun","binlerce", "sokaklar ikimizin","sevmek kimi zaman rezilce korkuluԁur","düş kurardık heyecanla", "değil mi insana", "bozulsun", "ölürüm","yaşta", "bal gibi"]),
+    choice(["acısı da", "aşk", "iyisi de", "nedendir ki","solması için gülü ԁalınԁan mı koparmalı","insan bir akşam üstü ansızın уorulur","başka","yangınımı sönԁürmeԁi kar benim","kimseye", "parlayıp da sönünce ışık ", "zalim","biliyoruz"]),
+    choice(["film çekilir gibi bak","sus ԁeуip aԁınla başlıуorum","ölԁürmek için silah, hançer mi olmalı","nedenler var","kimi zaman ellerini kırar tutkusu","özgür değiliz dünyada", "ritimleri bozuk","farkındalık neyse", "sokaklar ikimizin"]),
+    choice(["bak o sevgimizin ","ve ben çekip giԁerim bir nehir akıp giԁer","düşüyor","efsane olur","arkasınԁa уalnızlığın hınzır uğultusu","sen hem bir hastalık hem ԁe sağlık gibisin","saçlar bağ, gözler silah, gülüş, kurşun olamaz mı","parlayıp da sönünce ışık ","Ben sana mecburum bilemezsin","iyisi de", "nedendir ki", "başka", "kimseye","zalim", " biliyoruz", "sokaklar ikimizin","YORGAN GİTTİ, KAVGA BİTTİ","hoşçakal"])
+]
+
+erkek_ismi = input("Erkek ismi giriniz..:")
+kadin_ismi = input("Kadın ismi giriniz..:")
+print("1-15 arası mısra sayısı girebilirsiniz..")
+misra_sayisi = int(input("Yazdırmak istediğiniz mısra sayısını giriniz..:"))
+
+while True:
+    if misra_sayisi > 16 :
+        print("Geçersiz bir mısra sayısı girildi..")
+        print("Program kapatılıyor..")
+        break
+    elif misra_sayisi == 1 :
+        print(erkek_ismi, ",\n"" {} {} {} {} {} {}.".format(*kelimeler))
+        break
+    elif misra_sayisi == 2 :
+        print(erkek_ismi, ",\n"" {} {} {} {},\n {} {} {} {} {}.".format(*kelimeler))
+        break
+    elif misra_sayisi == 3 :
+        print(erkek_ismi, ",\n"" {} {} {} {},\n {} {} {} {},\n {} {} {} {}.".format(*kelimeler))
+        break
+    elif misra_sayisi == 4 :
+        print(erkek_ismi, ",\n"" {} {} {} {},\n {} {} {},\n {} {} {},\n {} {} {}.".format(*kelimeler))
+        break
+    elif misra_sayisi == 5 :
+        print(erkek_ismi, ",\n"" {} {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {}.".format(*kelimeler))
+        break
+    elif misra_sayisi == 6 :
+        print(erkek_ismi, ",\n"" {} {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {}.".format(*kelimeler))
+        break
+    elif misra_sayisi == 7:
+        print(erkek_ismi, ",\n"" {} {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {},\n {} {}.".format(*kelimeler))
+        break
+    elif misra_sayisi == 8:
+        print(erkek_ismi, ",\n"" {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {},\n {} {},\n {} {}.".format(*kelimeler))
+        break
+    elif misra_sayisi == 9:
+        print(erkek_ismi, ",\n"" {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {},\n {} {},\n {} {},\n {} {}.".format(*kelimeler))
+        break
+    elif misra_sayisi == 10:
+        print(erkek_ismi, ",\n"" {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {}.".format(*kelimeler))
+        break
+    elif misra_sayisi == 11:
+        print(erkek_ismi,",\n"" {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {}.".format(*kelimeler))
+        break
+    elif misra_sayisi == 12:
+        print(erkek_ismi,",\n"" {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {}.".format(*kelimeler))
+        break
+    elif misra_sayisi == 13:
+        print(erkek_ismi,",\n"" {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {}.".format(*kelimeler))
+        break
+    elif misra_sayisi == 14:
+        print(erkek_ismi,",\n"" {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {}.".format(*kelimeler))
+        break
+    elif misra_sayisi == 15:
+        print(erkek_ismi,",\n"" {} {} {},\n {} {} {},\n {} {},\n {} {} {},\n {} {} {},\n {} {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {},\n {} {}.".format(*kelimeler))
+        break
+    else :
+        print("Hatalı Giriş Yapıldı...")
+        print("Program kapatılıyor...")
+        break
