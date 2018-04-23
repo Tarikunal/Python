499d766702367f33dfca5e1afaea367c9160aa47
Tarık Ünal <36507033+Tarikunal@users.noreply.github.com>
2018-04-23 11:48:17 +0300

    Create entry.py

---
+++ Tkinter/entry.py
@@ -0,0 +1,11 @@
+from tkinter import *
+ana = Tk()
+def islem():
+   cek = giris.get()
+   print (cek)
+   yaz = Label(ana,text='Yazı : '+cek).pack()
+giris = Entry()
+giris.pack()
+bas = Button(ana,text='Bas',command=islem).pack()
+cikis =Button(ana,text='Çıkış',command=ana.destroy).pack()
+ana.mainloop()
