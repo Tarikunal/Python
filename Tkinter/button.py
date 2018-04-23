d54d074152c0c85aa700b0d195cb55e03cf4fe63
Tarık Ünal <36507033+Tarikunal@users.noreply.github.com>
2018-04-23 11:56:47 +0300

    Create button.py

---
+++ Tkinter/button.py
@@ -0,0 +1,23 @@
+-import tkinter as tk	
+-	
+-	
+-class MyButtons:	
+-	
+-    def __init__(self, master):	
+-        frame = tk.Frame(master)	
+-        frame.pack()	
+-	
+-        self.printButton = tk.Button(frame, text="Mesajı yazdır", command=self.printMessage)	
+-        self.printButton.pack(side=tk.LEFT)	
+-	
+-        self.quitButton = tk.Button(frame, text="Çıkış", command=frame.quit)	
+-        self.quitButton.pack(side=tk.LEFT)	
+-	
+-    def printMessage(self):	
+-        print('This things works!!!')	
+-	
+-	
+-root = tk.Tk()	
+-byButts = MyButtons(root)	
+-	
+-root.mainloop()
